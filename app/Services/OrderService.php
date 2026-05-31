b130e49f96d494073db93505e9e783d4133d9f41
kutycma <khacquang2010@gmail.com>
2026-05-31 21:11:47 +0700

    Update OrderService.php

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\DB;
 class OrderService
 {
     CONST STR_TO_TIME = [
-        'day_price' => 0.033,
+        // 'day_price' => 0.033,
         'month_price' => 1,
         'quarter_price' => 3,
         'half_year_price' => 6,
